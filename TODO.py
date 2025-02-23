d4026533d817d85f3b5f82fadf464a4d498fe8a8
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-02-23 23:32:44 +0000

    Put all TODO into a single TODO file

---
+++ TODO.py
@@ -0,0 +1,16 @@
+
+# TODO: Make "types" of questions, as in 0: "Multiple Choice", 1: "True/False", 2: Input, etc...
+# TODO: Make it so that when an answer is false, it doesn't get validated on "prompt" and the user can try again (but still, the variable "correct_on_first_try" keeps it's purpose).
+# TODO: Make sure question answers don't repeat (review generating questions)
+# TODO: More questions. And more "manually" created questions for pattern-recognition testing.
+# TODO: More subjects (Set theory, logic, etc... Khan academy for inspiration)
+# TODO: Programming subjects maybe? idk
+# TODO: Add a "score" system, where the user can see how well they did on each subject (db.json file)
+# TODO: Add a "difficulty" system, where the user can choose the difficulty of the questions from Easy to Medium to Hard to Evil (either the questions get more compactly complex, not just too long, or just the subject gets more complicated like 1+1 -> 4-8 + 2/2) (Also saved on db.json file)
+# TODO: Give the ability to stop the quiz at any time/skip a question and get marked as false
+# TODO: Show the detailed answer to the user after they answer/skip the question
+# TODO: Add a "hint" system, where the user can ask for a hint on the question
+# TODO: Add a "skip" system, where the user can skip the question
+# TODO: Lessons system, where the user can learn the subject before taking the quiz
+# TODO: General Math Quiz, where the user can get quizzed on general questions (systems of questions, real-world geometry problems)
+# TODO: Bookark system, where the user can bookmark a subject 
