e537cb1200f36cf6b1e01611b8b2cb941c3aca29
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-02-25 23:27:19 +0000

    Added number of questions
    Will be developped later

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import src.defs as defs
 
 # Clear the console
-defs.clear_console()
+# defs.clear_console()
 while True:
     # Get questions (asking the user recursively for the folder until the gen.py is found)
-    questions = defs.get_questions(5)
+    questions = defs.get_questions(7)
     # Initialise the quiz
     quiz = defs.Quiz(questions)
     # Start the quiz
@@ -12,3 +12,4 @@ while True:
     # Ask the user if they want to continue
     if not defs.ask_if_stop():
         break
+
