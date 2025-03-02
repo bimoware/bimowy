f165a5033f7e850e7522adba6d40fba68f3f017b
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-02 21:27:16 +0000

    Create addition.ts

---
+++ server/subjects/addition.ts
@@ -0,0 +1,25 @@
+export default {
+	type: "subject",
+	id: "add",
+	name: "Addition",
+	description: "Taking the sum of two (or more) numbers",
+	questionGenerator: [
+		(difficulty: difficulty) => {
+			const ranges = {
+				easy: [1, 10],
+				medium: [10, 1e2],
+				hard: [1e2, 1e3],
+				evil: [1e5, 1e10]
+			};
+			const range = ranges[difficulty];
+			const [n1, n2] = [range, range].map((r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]);
+			return [
+				{
+					type: "input",
+					title: n1 + " + " + n2,
+					answer: n1 + n2
+				}
+			];
+		}
+	]
+};
