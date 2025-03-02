390d2924bdb293a539a50f8a4e6c9db2d5c7dffb
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-02 21:23:53 +0000

    Apply conditionally an <a> tag around an element if the href exists

---
+++ client/src/components/ConditionalHref.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export default function ConditionalHref({ href, children }: { href?: string, children: ReactNode }) {
+    return !href
+        ? children
+        : <a href={href}>{children}</a>
+}
