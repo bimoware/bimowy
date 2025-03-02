49b467f04560d10c114ef0c2cb703dabd2cb28ef
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-02 21:25:15 +0000

    Adopted react-router instead of react-router-dom for more intuitive UX

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import RouterManager from './router.tsx';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './style.css';
+import DashboardPage from './pages/DashboardPage.tsx';
+import SideBar from './components/SideBar.tsx';
+import PageSection from './components/PageSection.tsx';
+import TestPage from './pages/TestPage.tsx';
+import QuizPage from './pages/QuizPage.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
   <StrictMode>
-    <RouterManager/>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <SideBar />
+      <PageSection className="w-full p-6">
+        <Routes>
+          <Route index element={<TestPage />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="quiz">
+            <Route path=":quizid" element={<QuizPage />} />
+          </Route>
+        </Routes>
+      </PageSection>
+    </BrowserRouter>
+  </StrictMode>
+);
