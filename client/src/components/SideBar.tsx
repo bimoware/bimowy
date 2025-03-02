854205edb5db41520e1ab1cf5290fef17004e140
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-02 21:24:21 +0000

    Gave the sidebar a minimum width to avoid extreme flex conflicts

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,14 +1,31 @@
+import TestPage from "../pages/TestPage";
 import PageSection from "./PageSection";
 import SideBarIcon from "./SideBarIcon";
 
-import { routes } from '../router.tsx'
+import testIcon from '/svgs/test.svg';
+import dashboardIcon from '/svgs/dashboard.svg';
+import DashboardPage from "../pages/DashboardPage";
+
+const routes = [
+    {
+        id: "test",
+        element: <TestPage />,
+        icon: testIcon,
+        href: "/"
+    },
+    {
+        id: "dashboard",
+        element: <DashboardPage />,
+        icon: dashboardIcon,
+    }
+].map(route => ({ ...route, href: route.href || ("/" + route.id) }))
 
 export default function SideBar() {
     const currentPath = window.location.pathname;
-    return <PageSection className="flex flex-col gap-3">
+    return <PageSection className="flex flex-col gap-3 min-w-17">
         {
-            routes.map(({ id, icon, href }, key) => <SideBarIcon
-                {...{ key, id, icon, currentPath, href }} />
+            routes.map(({ id, icon, href }) => <SideBarIcon
+                {...{ id, icon, currentPath, href }} key={id} />
             )
         }
     </PageSection>
