5c1afcd41776a675f1713d29eef29a519f73d0e1
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-02 21:28:00 +0000

    Added subjects on test-page

--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -1,19 +1,28 @@
 import { useEffect, useState } from "react";
 import LoadingIcon from "../components/LoadingIcon";
+import { SubjectBloc } from "./DashboardPage";
+import { subject } from "../defs";
+
+
 
 export default function TestPage() {
-    const [data, setData] = useState<null | string[]>(null);
+    const [data, setData] = useState<null | subject[]>(null);
 
     useEffect(() => {
-        fetch("http://localhost:5000/api/")
+        fetch("http://localhost:5000/subjects/")
             .then((res) => res.json())
-            .then((data: string[]) => setData(data))
+            .then((data: subject[]) => setData(data))
             .catch((err) => console.error("Error:", err));
     }, []);
 
-    return <span>{
-        !data
-            ? <LoadingIcon/>
-            : data.map(name => <h2>{name}</h2>)
-    }</span>
+    return data
+        ? <div className="flex gap-4">
+            {data.map(subject => <SubjectBloc
+                href={`/quiz/${subject.id}`}
+                key={subject.id}
+                title={subject.name}
+                content={subject.description}
+            />)}
+        </div>
+        : <LoadingIcon />
 }
