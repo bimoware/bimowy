f9a33da6a2051b66352cc2e900b89290821551cb
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-03 17:34:54 +0000

    Fixed substraction.ts format

--- server/subjects/substraction.ts
+++ server/subjects/substraction.ts
@@ -2,5 +2,24 @@ export default {
     type: "subject",
 	id: "substract",
 	name: "Substraction",
-	description: "Taking a number and removing from it other number(s)"
+	description: "Taking a number and removing from it other number(s)",
+	questionGenerator: [
+		(difficulty: difficulty) => {
+			const ranges = {
+				easy: [1, 10],
+				medium: [10, 1e2],
+				hard: [1e2, 1e3],
+				evil: [1e5, 1e10]
+			};
+			const range = ranges[difficulty];
+			const [n1, n2] = [range, range].map((r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]);
+			return [
+				{
+					type: "input",
+					title: n1 + " - " + n2,
+					answer: n1 - n2
+				}
+			];
+		}
+	]
 };
