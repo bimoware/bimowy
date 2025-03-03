60e92663552918553521cc7557044a8d8c4e4a1a
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-03 17:22:31 +0000

    Added a /subjects route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import SideBar from './components/SideBar.tsx';
 import PageSection from './components/PageSection.tsx';
 import TestPage from './pages/TestPage.tsx';
 import QuizPage from './pages/QuizPage.tsx';
+import SubjectsPage from './pages/SubjectsPage.tsx';
 
 const root = document.getElementById("root")!;
 
@@ -18,6 +19,7 @@ ReactDOM.createRoot(root).render(
         <Routes>
           <Route index element={<TestPage />} />
           <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="subjects" element={<SubjectsPage />} />
           <Route path="quiz">
             <Route path=":quizid" element={<QuizPage />} />
           </Route>
