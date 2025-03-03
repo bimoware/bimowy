555a26b5aa9da3a722b85393338fb71c2fe7101d
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-03 17:23:16 +0000

    Removed TestPage content -> SubjectsPage

--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -1,28 +1,5 @@
-import { useEffect, useState } from "react";
-import LoadingIcon from "../components/LoadingIcon";
-import { SubjectBloc } from "./DashboardPage";
-import { subject } from "../defs";
-
 
 
 export default function TestPage() {
-    const [data, setData] = useState<null | subject[]>(null);
-
-    useEffect(() => {
-        fetch("http://localhost:5000/subjects/")
-            .then((res) => res.json())
-            .then((data: subject[]) => setData(data))
-            .catch((err) => console.error("Error:", err));
-    }, []);
-
-    return data
-        ? <div className="flex gap-4">
-            {data.map(subject => <SubjectBloc
-                href={`/quiz/${subject.id}`}
-                key={subject.id}
-                title={subject.name}
-                content={subject.description}
-            />)}
-        </div>
-        : <LoadingIcon />
+    return <></>
 }
