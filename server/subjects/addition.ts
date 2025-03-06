06ee5b3f2ca65520de65362454daeac30f2cbea7
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-06 19:15:42 +0000

    Modified "subject" format
    Now, multiple question types for the same exercice can exist, each with a way to generate random values and another to format them (a format function in case I already have pre-disposed values.)

--- server/subjects/addition.ts
+++ server/subjects/addition.ts
@@ -1,28 +1,36 @@
-const getAnswer = (n1: number, n2: number) => n1 + n2;
+const getAnswer = ([n1, n2]: number[]) => n1 + n2;
 
 export default {
 	type: "subject",
-	id: "add",
-	name: "Addition",
-	description: "Taking the sum of two (or more) numbers",
-	questionGenerator: [
-		(difficulty: difficulty) => {
-			const ranges = {
-				easy: [1, 10],
-				medium: [10, 1e2],
-				hard: [1e2, 1e3],
-				evil: [1e5, 1e10]
-			};
-			const range = ranges[difficulty];
-			const [n1, n2] = [range, range].map((r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]);
-			return [
-				{
-					type: "input",
-					title: n1 + " + " + n2,
-					answer: getAnswer(n1, n2)
-				}
-			];
+	id: "substract",
+	name: "Substraction",
+	description: "Taking the sum of multiple numbers",
+	questionVariations: [
+		{
+			id: "basic",
+			getAnswer,
+			generate: (difficulty: difficulty) => {
+				const ranges = {
+					easy: [1, 10],
+					medium: [10, 1e2],
+					hard: [1e2, 1e3],
+					evil: [1e5, 1e10]
+				};
+				const range = ranges[difficulty];
+				const [n1, n2] = [range, range].map(
+					(r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]
+				);
+				return [n1, n2];
+			},
+			format: ([n1, n2]: number[]) => {
+				return [
+					{
+						type: "input",
+						title: n1 + " + " + n2,
+						answer: getAnswer([n1, n2])
+					}
+				];
+			}
 		}
-	],
-	getAnswer
+	]
 };
--- server/subjects/substraction.ts
+++ server/subjects/substraction.ts
@@ -1,28 +1,36 @@
-const getAnswer = (n1: number, n2: number) => n1 - n2;
+const getAnswer = ([n1, n2]: number[]) => n1 - n2;
 
 export default {
-    type: "subject",
+	type: "subject",
 	id: "substract",
 	name: "Substraction",
 	description: "Taking a number and removing from it other number(s)",
-	questionGenerator: [
-		(difficulty: difficulty) => {
-			const ranges = {
-				easy: [1, 10],
-				medium: [10, 1e2],
-				hard: [1e2, 1e3],
-				evil: [1e5, 1e10]
-			};
-			const range = ranges[difficulty];
-			const [n1, n2] = [range, range].map((r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]);
-			return [
-				{
-					type: "input",
-					title: n1 + " - " + n2,
-					answer: getAnswer(n1,n2)
-				}
-			];
+	questionVariations: [
+		{
+			id: "basic",
+			getAnswer,
+			generate: (difficulty: difficulty) => {
+				const ranges = {
+					easy: [1, 10],
+					medium: [10, 1e2],
+					hard: [1e2, 1e3],
+					evil: [1e5, 1e10]
+				};
+				const range = ranges[difficulty];
+				const [n1, n2] = [range, range].map(
+					(r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]
+				);
+				return [n1, n2];
+			},
+			format: ([n1, n2]: number[]) => {
+				return [
+					{
+						type: "input",
+						title: n1 + " - " + n2,
+						answer: getAnswer([n1, n2])
+					}
+				];
+			}
 		}
-	],
-	getAnswer
+	]
 };
