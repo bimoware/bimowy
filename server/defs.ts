99a3a8318f40cddb8194cfca47beab97bd20a9db
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-06 20:00:23 +0000

    Created types, enums and a class for ExerciceGenerator for simplicity

--- server/defs.ts
+++ server/defs.ts
@@ -1,8 +1,56 @@
-type difficulty = "easy" | "medium" | "hard" | "evil"
-type subject = {
-    type: string,
-    id: string,
-    name: string,
-    description: string,
-    questionGenerator: Function[]
+export enum Difficulty {
+	Easy = 0,
+	Medium = 1,
+	Hard = 2,
+	Evil = 3
+}
+
+export enum QuestionPartType {
+	Text = 0,
+	Input = 1
+}
+
+export type questionPart = {
+	type: "input" | "text";
+	answer: number;
+	title?: string;
+};
+
+export type QuestionGenerator = {
+    id: string;
+    getAnswer: (inputs: number[]) => number;
+    generate: (difficulty: Difficulty) => number[];
+    format: (inputs: number[]) => questionPart[];
+}
+
+export type ExerciceGeneratorData = {
+	id: string;
+	name: string;
+	description: string;
+	questionGenerators: QuestionGenerator[]
+};
+
+export class ExerciceGenerator {
+	questionGenerators: QuestionGenerator[]
+    id: string;
+    name: string;
+    description: string;
+	constructor(data: ExerciceGeneratorData) {
+        this.id = data.id
+        this.name = data.name
+        this.description = data.description
+        this.questionGenerators = data.questionGenerators
+	}
+
+	generateRandomQuestion({ difficulty }: { difficulty: Difficulty }) {
+		const questionI = Math.floor(Math.random() * this.questionGenerators.length);
+		const questionGenerator = this.questionGenerators[questionI];
+		const question = questionGenerator.generate(difficulty);
+		return question;
+	}
+	validateAnswer({ id, params, answer }: { id: string; params: number[]; answer: number }) {
+		const question = this.questionGenerators.find((q) => q.id === id);
+		if (!question) return new Error(`Question w/ id "${id}" not found`);
+		return question.getAnswer(params) === answer;
+	}
 }
