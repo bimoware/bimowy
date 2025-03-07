eaefb93562e876c3c7c5f66836f1dada374cf44e
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-07 17:26:25 +0000

    Restarting from scratch

--- server/server.ts
+++ server/server.ts
@@ -1,29 +1,30 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
-import { ExerciceGenerator } from "./defs";
+import { ExerciceRessource, Ressource } from "./defs";
 
 const app = express();
-const PORT = 60001;
-const subjects = new Map<string, ExerciceGenerator>();
+const PORT = 4005;
+const ressources = new Map<string, Ressource>();
 
 app.use(cors());
 
 function fetchSubjects() {
-	for (let file of fs.readdirSync("./subjects")) {
-		let data = require("./subjects/" + file).default;
-		subjects.set(data.id, data);
+	console.log("⬜ Fetching ressources...");
+	ressources.clear()
+	for (let file of fs.readdirSync("./ressources")) {
+		let data: Ressource = require("./ressources/" + file).default;
+		ressources.set(data.id, data);
 	}
+	console.log("🟩 Ressources fetched!");
 }
 
 fetchSubjects();
 
-app.get("/subjects/", (req, res) => {
-	console.log(subjects)
-	// res.json('ok')
-	res.json(Array.from(subjects.values()));
+app.get("/api/", (req, res) => {
+	res.json(Array.from(ressources.values()));
 });
 
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+	console.log(`🌐 Server running on port http://localhost:${PORT}/api`);
 });
