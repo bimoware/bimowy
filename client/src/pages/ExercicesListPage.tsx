c586dbc2d5b5935f7083366834ca2939c5d803c8
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-07 11:46:02 +0000

    Renamed SubjectsPage

--- client/src/pages/ExercicesListPage.tsx
+++ client/src/pages/ExercicesListPage.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import LoadingIcon from "../components/LoadingIcon";
-import { SubjectBloc } from "./DashboardPage";
+import { Card } from "./DashboardPage";
 import { subject } from "../defs";
 
 
-export default function SubjectsPage() {
+export default function ExercicesListPage() {
     const [data, setData] = useState<null | subject[]>(null);
 
     useEffect(() => {
@@ -16,8 +16,8 @@ export default function SubjectsPage() {
 
     return data
         ? <div className="flex gap-4">
-            {data.map(subject => <SubjectBloc
-                href={`/quiz/${subject.id}`}
+            {data.map(subject => <Card
+                href={`/exercice/${subject.id}`}
                 key={subject.id}
                 title={subject.name}
                 content={subject.description}
