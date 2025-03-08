7d54b68e59c4b0ed3cd06a9a6c980e8c32a330db
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-08 00:01:02 +0000

    Removed useless logs & added /api/ gate to every port

--- server/server.ts
+++ server/server.ts
@@ -4,27 +4,41 @@ import fs from "fs";
 import { ExerciceRessource, Ressource } from "./defs";
 
 const app = express();
-const PORT = 4005;
+const PORT = 1230;
 const ressources = new Map<string, Ressource>();
 
 app.use(cors());
 
 function fetchSubjects() {
-	console.log("⬜ Fetching ressources...");
 	ressources.clear();
 	for (let file of fs.readdirSync("./ressources")) {
 		let data: Ressource = require("./ressources/" + file).default;
 		ressources.set(data.id, data);
 	}
-	console.log("🟩 Ressources fetched!");
 }
 
 fetchSubjects();
 
-app.get("/api/", (req, res) => {
+app.listen(PORT, () => {
+	console.log(`🌐 Server running on port http://localhost:${PORT}`);
+});
+
+app.get("/api/ressources", (req, res) => {
 	res.json(Array.from(ressources.values()));
 });
 
-app.listen(PORT, () => {
-	console.log(`🌐 Server running on port http://localhost:${PORT}/api`);
+app.get("/api/exercice/:exercice_id", (req, res) => {
+	const { exercice_id } = req.params;
+	const difficulty = Number(req.query["difficulty"]) || 0;
+	const exercice = ressources.get(exercice_id);
+	if (!exercice) {
+		res.status(404).send(`Exercice with ID '${exercice_id}' not found`);
+	} else if (!(exercice instanceof ExerciceRessource)) {
+		res.status(400).send("Exercice is not an exercice ressource");
+	} else {
+		const questions = Array.from({ length: 3 }).map((_, i) => ({
+			parts: exercice.generateRandomQuestion(difficulty)
+		}));
+		res.json(questions);
+	}
 });
