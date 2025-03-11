37469746322072662923c48e7ea01e6d000655b0
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-11 03:44:57 +0000

    Applied defs.ts directly

--- client/src/pages/RessourcesPage.tsx
+++ client/src/pages/RessourcesPage.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react'
 import LoadingIcon from '../components/LoadingIcon'
 import { Card } from './DashboardPage'
-import { subject } from '../defs'
+
+type subject = {
+  name: string
+  id: string
+  desc: string
+}
 
 export default function RessourcesPage() {
-  const [data, setData] = useState<null | subject[]>(null)
+  const [data, setData] = useState<subject[]>([])
 
   useEffect(() => {
     fetch('http://localhost:1230/api/ressources')
       .then((res) => res.json())
       .then((data: subject[]) => setData(data))
-      .catch((err) => console.error('Error:', err))
   }, [])
 
-  return !data ? (
+  return !data.length ? (
     <LoadingIcon />
   ) : (
     <div className='flex gap-4'>
@@ -22,7 +26,7 @@ export default function RessourcesPage() {
           href={`/exercice/${subject.id}`}
           key={subject.id}
           title={subject.name}
-          content={subject.description}
+          content={subject.desc}
         />
       ))}
     </div>
