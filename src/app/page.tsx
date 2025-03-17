01b3888158352bbb366c71c09bac21525f9b3f7a
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-17 23:53:00 +0000

    Formatting improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: { default: "Home", template: "%s | Bimowy" },
+  title: {
+    default: "Home",
+    template: "%s | Bimowy"
+  },
   description: "Home Page",
   icons: "/svgs/home.svg"
 };
 
 export default function Home() {
-  return <>[Body]</>
+  return <>
+    <h1>Bimowy</h1></>
 }
