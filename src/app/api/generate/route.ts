17b189be8a6ab1bda0fdc9529e71f3d0b041c8f6
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-03-18 01:06:23 +0000

    Create route.ts

---
+++ src/app/api/generate/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import db from '../exercices/db'
+
+// To handle a POST request to /api/generate-exercices
+export async function POST(req: Request) {
+	try {
+		// Parse the incoming JSON body
+		const { exercice_id, n } = await req.json()
+		const exercice = db.find((ex) => ex.id === exercice_id) 
+
+		if (!exercice) {
+			return NextResponse.json(
+				{ message: `Exercice with ID '${exercice_id}' not found` },
+				{ status: 404 }
+			)
+		} else {
+			const questions = Array.from({ length: n || 5 }).map(() =>
+				exercice.generate()
+			)
+			return NextResponse.json(questions)
+		}
+	} catch (error) {
+		console.error(error)
+		return NextResponse.json(
+			{ message: 'Invalid request format' },
+			{ status: 400 }
+		)
+	}
+}
