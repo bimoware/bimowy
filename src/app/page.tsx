5c1f8faacc9e738ab6b2530fbf012308d98864b7
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 08:11:07 +0000

    Final working test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,14 @@ export const metadata: Metadata = {
   icons: "/svgs/home.svg",
   description: "Bimowy is a free, open-source, exercices-focused math platform for students who feel stuck when trying to train on any math subject."
     + " With interactive learning and feedback loop, math becomes free from frustration and starts being actually fun.",
-  metadataBase: new URL(process.env.NEXT_PUBLIC_VERCEL_URL
-    ? ("https://" + process.env.NEXT_PUBLIC_VERCEL_URL)
-    : 'http://localhost:3000/')
+  metadataBase: new URL(
+    process.env.NODE_ENV == "production"
+      ? `https://bimowy.vercel.app/`
+      : 'http://localhost:3000/'
+  ),
+  twitter: {
+    card: "summary"
+  }
 };
 
 export const viewport: Viewport = { themeColor: "#FFFFFE" }
