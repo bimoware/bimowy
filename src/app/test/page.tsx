fed4a436e78c9b1455a52bf2302d1a8e766cb155
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 04:27:23 +0000

    Emptied the test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -10,15 +10,5 @@ export const metadata: Metadata = {
 };
 
 export default function TestPage() {
-  return <div className="grow flex m-4 gap-4">
-    <div className="grow flex flex-col gap-5">
-      <h1 className="mt-2 ml-4">Addition - Question 4/10</h1>
-      <div className="grow flex bg-neutral-900 rounded-3xl p-4">
-        [...Context]
-      </div>
-    </div>
-    <div className="grow flex bg-neutral-900 rounded-3xl p-4">
-      [...Inputs]
-    </div>
-  </div>
+  return <></>
 }
