1b6decf0dc76ddada283bc4f07e99d72868219c6
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 01:04:22 +0000

    Added edge cases for generating exercises

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,29 +1,61 @@
 import { NextResponse } from 'next/server'
 import db from '../db'
 
-// To handle a POST request to /api/generate-exercices
 export async function POST(req: Request) {
 	try {
-		// Parse the incoming JSON body
-		const { exercice_id, n } = await req.json()
-		const exercice = db.find((ex) => ex.id === exercice_id)
-
-		if (!exercice) {
+		const { exercise_id, n } = await req.json()
+		if (!exercise_id)
 			return NextResponse.json(
-				{ message: `Exercice with ID '${exercice_id}' not found` },
+				{ message: 'exercise_id is required.' },
+				{ status: 400 }
+			)
+		if (typeof exercise_id !== 'string')
+			return NextResponse.json(
+				{ message: 'exercise_id must be a string.' },
+				{ status: 400 }
+			)
+		if (!db.find((ex) => ex.id === exercise_id))
+			return NextResponse.json(
+				{ message: `Exercise with ID '${exercise_id}' not found` },
+				{ status: 404 }
+			)
+		if (n) {
+			if (typeof n !== 'number')
+				return NextResponse.json(
+					{ message: 'n must be a number.' },
+					{ status: 400 }
+				)
+			if (n % 1 !== 0)
+				return NextResponse.json(
+					{ message: 'n must be an integer.' },
+					{ status: 400 }
+				)
+			if (n < 1 || n > 10)
+				return NextResponse.json(
+					{ message: 'n must be in range 1 to 10 (inclusive).' },
+					{ status: 400 }
+				)
+		}
+		const exercise = db.find((ex) => ex.id === exercise_id)
+
+		if (!exercise) {
+			return NextResponse.json(
+				{ message: `Exercise with ID '${exercise_id}' not found` },
 				{ status: 404 }
 			)
 		} else {
-			const questions = Array.from({ length: n || 3 }).map(() =>
-				exercice.generate()
+			const exercises = Array.from({ length: n || 3 }).map(() =>
+				exercise.generate()
 			)
-			return NextResponse.json(questions)
+			return NextResponse.json({ ...exercise, exercises }, { status: 200 })
 		}
 	} catch (error) {
 		console.error(error)
 		return NextResponse.json(
-			{ message: 'Invalid request format' },
-			{ status: 400 }
+			{
+				message: 'Internal errror. Sorry. We will look into it.'
+			},
+			{ status: 500 }
 		)
 	}
 }
