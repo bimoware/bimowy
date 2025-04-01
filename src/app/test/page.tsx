890aaf99288f9a0dfb5ff4b55e8cf3d3ed51754d
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 01:05:22 +0000

    New exercises page test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Test",
+    template: "%s | Bimowy"
+  },
+  description: "Test Page",
+  icons: "/svgs/test.svg"
+};
+
 export default function TestPage() {
-    return <></>
-} 
+  return <div className="grow flex m-4 gap-4">
+    <div className="grow flex flex-col gap-5">
+      <h1 className="mt-2 ml-4">Addition - Question 4/10</h1>
+      <div className="grow flex bg-neutral-900 rounded-3xl p-4">
+        [...Context]
+      </div>
+    </div>
+    <div className="grow flex bg-neutral-900 rounded-3xl p-4">
+      [...Inputs]
+    </div>
+  </div>
+}
