aec0c0ff82ab04831496fdeea4cf153efba7781a
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 08:03:55 +0000

    Test #5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { Metadata, Viewport } from "next";
 export const metadata: Metadata = {
   title: "Bimowy - When math works",
   icons: "/svgs/home.svg",
-  description: "Bimowy is a free open-source math practice platform for students who feel stuck when trying to train on any math subject. Through quiz-based exercises, learning becomes interactive, free from frustration and ACTUALLY fun. All to help you finally understand topics that once felt impossible. Whether you're dealing with math anxiety, burnout or just need extra practice, Bimowy gives you instant feedback to reinforce learning and boost your confidence. Built with **Next.js**, it offers a smooth and responsive experience designed for self-paced learning, homework help, and exam preparation. Stop feeling lost and start using Bimowy!",
+  description: "Bimowy is a free, open-source, exercices-focused math platform for students who feel stuck when trying to train on any math subject."
+    + " With interactive learning and feedback loop, math becomes free from frustration and starts being actually fun.",
   metadataBase: new URL(
     process.env.NODE_ENV == "production"
       ? `https://bimowy.vercel.app/`
