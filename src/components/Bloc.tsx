21a7755e86548572e42cd29ac4890017c710ab8d
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 04:27:15 +0000

    Removed flex property for anything other than the sidebar

--- src/components/Bloc.tsx
+++ src/components/Bloc.tsx
@@ -5,11 +5,10 @@ export function Bloc({ type, children }: {
     children: ReactNode
 }) {
     const styles = {
-        "sidebar": "p-2 flex-col items-baseline w-18 shrink-0 h-fit",
+        "sidebar": "p-2 flex flex-col items-baseline w-18  shrink-0 h-fit",
         "full-body": "p-7 w-full items-baseline"
     }
-    return <div className={`bg-neutral-900 m-4 rounded-3xl
-    flex overflow-y-scroll
+    return <div className={`bg-neutral-900 m-4 rounded-3xl overflow-y-scroll
      ${styles[type]}`}>
         {children}
     </div>
