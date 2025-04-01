09a59c8f28a85b3323276d3602748240179c6c93
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 05:09:13 +0000

    Fixed build error

--- src/app/exercises/[exercise_id]/layout.tsx
+++ src/app/exercises/[exercise_id]/layout.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from "next";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
     title: "Exercising...",
     icons: "/svgs/exercise.svg"
 };
 
-export default function Layout({ children }: { children: ChildNode }) { return children }
+export default function Layout({ children }: { children: ReactNode }) { return children }
