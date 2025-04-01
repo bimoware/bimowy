b8791a5c08b9876f5bab00fe74371c6b72d31761
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-01 07:43:51 +0000

    Test 2 at image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { Metadata, Viewport } from "next";
 export const metadata: Metadata = {
   title: "Bimowy - When math works",
   icons: "/svgs/home.svg",
-  description: "Bimowy is a free open-source math practice platform for students who feel stuck when trying to train on any math subject. Through quiz-based exercises, learning becomes interactive, free from frustration and ACTUALLY fun. All to help you finally understand topics that once felt impossible. Whether you're dealing with math anxiety, burnout or just need extra practice, Bimowy gives you instant feedback to reinforce learning and boost your confidence. Built with **Next.js**, it offers a smooth and responsive experience designed for self-paced learning, homework help, and exam preparation. Stop feeling lost and start using Bimowy!",
+  description: "1 | Bimowy is a free open-source math practice platform for students who feel stuck when trying to train on any math subject. Through quiz-based exercises, learning becomes interactive, free from frustration and ACTUALLY fun. All to help you finally understand topics that once felt impossible. Whether you're dealing with math anxiety, burnout or just need extra practice, Bimowy gives you instant feedback to reinforce learning and boost your confidence. Built with **Next.js**, it offers a smooth and responsive experience designed for self-paced learning, homework help, and exam preparation. Stop feeling lost and start using Bimowy!",
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-      : 'http://localhost:3000'
+    process.env.NODE_ENV == "production"
+      ? `https://bimowy.vercel.app/`
+      : 'http://localhost:3000/'
   ),
   openGraph: {
     images: "/media/logo_trans.png",
