27dacfeb13bf5f648398db0376f98143dd2b9896
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-02 08:52:10 +0000

    * Add multiplication exercise

--- src/app/api/db.ts
+++ src/app/api/db.ts
@@ -84,6 +84,48 @@ const db = [
 				}
 			]
 		}
+	),
+	new ExerciseResource(
+		'multiplication',
+		'Multiplication',
+		'Multiple two values together',
+		['basic-arithmetic'],
+		// validateAnswers
+		function (
+			[n1, n2]: number[],
+			[answer1]: {
+				id: string
+				value: string
+			}[]
+		) {
+			return [
+				{
+					id: answer1.id,
+					is_correct: String(answer1.value) === (n1 * n2).toString()
+				}
+			]
+		},
+		// generateSeed
+		function () {
+			const range = [1, 10]
+			const [n1, n2] = [range, range].map(
+				(r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]
+			)
+			return [n1, n2]
+		},
+		// getContext
+		function ([n1, n2]: number[]) {
+			return [`${n1} * ${n2} = ?`]
+		},
+		// getInputs
+		function () {
+			return [
+				{
+					id: 'answer',
+					type: 'number'
+				}
+			]
+		}
 	)
 ]
 
