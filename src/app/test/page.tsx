9f5fe8817847fc7f4bb4951521ddebb7f21ad65e
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-03 08:35:18 +0000

    Stupid tests who cares

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,39 @@
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: {
-    default: "Test",
-    template: "%s | Bimowy"
-  },
-  description: "Test Page",
-  icons: "/svgs/test.svg"
-};
+import { useState } from "react";
+import { ArrowContainer, Popover } from "react-tiny-popover";
 
 export default function TestPage() {
-  return <></>
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  return (
+    <Popover
+      isOpen={isPopoverOpen}
+      positions={['right', 'top', 'bottom']}
+      onClickOutside={() => setIsPopoverOpen(false)}
+      padding={10}
+      content={({ position, childRect, popoverRect }: {
+        position: 'left' | 'right' | 'top' | 'bottom',
+        childRect: DOMRect,
+        popoverRect: DOMRect
+      }) => (
+        <ArrowContainer
+          position={position}
+          childRect={childRect}
+          popoverRect={popoverRect}
+          arrowColor={'blue'}
+          arrowSize={10}
+          arrowStyle={{ opacity: 0.7 }}
+        >
+          <span>Ok</span>
+        </ArrowContainer>
+      )}
+    >
+      {/* Trigger element that opens the popover */}
+      <span
+        onMouseEnter={() => setIsPopoverOpen(true)}
+        onMouseLeave={() => setIsPopoverOpen(false)}
+      >Hey</span>
+    </Popover>
+  );
 }
