5c2141a15b59cd54f94a481fd9ddd36ee4c46157
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-03 08:35:42 +0000

    Added a layout so test/page can "use client"

---
+++ src/app/test/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Test",
+    template: "%s | Bimowy"
+  },
+  description: "Test Page",
+  icons: "/svgs/test.svg"
+};
+
+
+export default function Layout({ children }: { children: ReactNode }) { return children }
