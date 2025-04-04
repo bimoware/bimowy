82038434bb0ae462519b08c1600cd2afb61932e0
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-04 15:32:47 +0000

    Added inline-flex to all ligns

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -5,21 +5,21 @@ import Link from "next/link";
 
 export default function CreditsPage() {
     return <div className="text-4xl w-full h-full items-center justify-center gap-2 flex flex-col">
-        <div className="flex gap-2 items-center justify-center">
+        <div className="inline-flex gap-2 items-center justify-center">
             <span>Made with</span>
-            <Image src={"/svgs/heart.svg"} width={30} height={30} alt="Heart" className="h-fit w-fit aspect-square" />
+            <Image src={"/svgs/heart.svg"} width={30} height={30} alt="Heart" className="aspect-square" />
             <span>by</span>
             <Mention icon={"/media/pfp.jpeg"} name="bimoware" href={"https://github/bimoware/"} />
         </div>
-        <div className="flex gap-2 items-center justify-center">
+        <div className="inline-flex gap-2 items-center justify-center">
             <span>Source code on </span>
             <Mention icon={"/svgs/github.svg"} background name="bimoware/bimowy" href={"https://github.com/bimoware/bimowy/"} />
         </div>
-        <div className="flex gap-2 items-center justify-center">
+        <div className="inline-flex gap-2 items-center justify-center">
             <span>Inspired by</span>
             <Mention icon={"/svgs/khan-academy.svg"} name="Khan Academy" href={"https://www.khanacademy.org/"} />
         </div>
-        <div className="flex gap-2 items-center justify-center">
+        <div className="inline-flex gap-2 items-center justify-center">
             <span>Frameworks used are</span>
             <Mention icon={"/svgs/nextjs.svg"} name="Next.Js" href={"https://nextjs.org/"} />
             <span>and</span>
@@ -33,7 +33,7 @@ function Mention({ icon, name, href, background, padding }: { icon: string, name
         <div className="inline-flex gap-2 items-center p-1 px-2 rounded-2xl
         transition-all
         bg-neutral-700/70 hover:bg-neutral-700
-        select-none cursor-pointer hover:scale-102 hover:-rotate-1">
+        select-all cursor-pointer hover:scale-102 hover:-rotate-1">
             <Image src={icon} width={70} height={70} alt={name}
                 className={`h-8 w-fit aspect-square rounded-full
                 ${background && "bg-black"} ${padding && "p-1"}`} />
