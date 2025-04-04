51d0081fdf3a6114b4816a6b25c546f7426df69c
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-04 10:52:15 +0000

    Improved size of text & images of credits page

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function CreditsPage() {
-    return <div className="text-2xl w-full h-full items-center justify-center gap-2 flex flex-col">
+    return <div className="text-4xl w-full h-full items-center justify-center gap-2 flex flex-col">
         <div className="flex gap-2 items-center justify-center">
             <span>Made with</span>
             <Image src={"/svgs/heart.svg"} width={30} height={30} alt="Heart" className="h-fit w-fit aspect-square" />
@@ -13,32 +13,33 @@ export default function CreditsPage() {
         </div>
         <div className="flex gap-2 items-center justify-center">
             <span>Source code on </span>
-            <Mention icon={"/svgs/github.svg"} name="bimoware/bimowy" href={"https://github.com/bimoware/bimowy/"} />
+            <Mention icon={"/svgs/github.svg"} background name="bimoware/bimowy" href={"https://github.com/bimoware/bimowy/"} />
         </div>
         <div className="flex gap-2 items-center justify-center">
             <span>Inspired by</span>
             <Mention icon={"/svgs/khan-academy.svg"} name="Khan Academy" href={"https://www.khanacademy.org/"} />
         </div>
         <div className="flex gap-2 items-center justify-center">
-            <span>Made using </span>
+            <span>Frameworks used are</span>
             <Mention icon={"/svgs/nextjs.svg"} name="Next.Js" href={"https://nextjs.org/"} />
             <span>and</span>
-            <Mention icon={"/svgs/tailwind.svg"} padding name="TailwindCSS" href={"https://tailwindcss.com/docs/installation/framework-guides/nextjs/"} />
+            <Mention icon={"/svgs/tailwind.svg"} background padding name="TailwindCSS" href={"https://tailwindcss.com/docs/installation/framework-guides/nextjs/"} />
         </div>
     </div>
 }
 
-function Mention({ icon, name, href, padding }: { icon: string, name: string, href: string, padding?: boolean }) {
+function Mention({ icon, name, href, background, padding }: { icon: string, name: string, href: string, background?: boolean, padding?: boolean }) {
     return <Link href={href} target='_blank' >
-        <div className="inline-flex gap-2 items-center p-1 px-2 rounded-xl
+        <div className="inline-flex gap-2 items-center p-1 px-2 rounded-2xl
         transition-all
-        bg-neutral-600/50 hover:bg-neutral-600
-        select-none cursor-pointer hover:scale-102">
-            <Image src={icon} width={50} height={50} alt={name}
-                className={`h-6 w-fit aspect-square rounded-full bg-black ${padding ? "p-1" : ""}`} />
+        bg-neutral-700/70 hover:bg-neutral-700
+        select-none cursor-pointer hover:scale-102 hover:-rotate-1">
+            <Image src={icon} width={70} height={70} alt={name}
+                className={`h-8 w-fit aspect-square rounded-full
+                ${background && "bg-black"} ${padding && "p-1"}`} />
             <span className="font-bold">{name}</span>
             <Image src={"/svgs/open_in_view.svg"} width={50} height={50} alt="Open in new tab"
-                className="h-4 w-fit aspect-square self-baseline" />
+                className="h-4 w-fit aspect-square self-baseline -ml-1" />
         </div>
     </Link>
 }
