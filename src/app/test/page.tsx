03dcb86c91f1e65fe22bc3567fc78516384666d4
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-04 06:37:25 +0000

    Random testing stuff

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,39 +1,7 @@
-"use client";
-
-import { useState } from "react";
-import { ArrowContainer, Popover } from "react-tiny-popover";
-
 export default function TestPage() {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-
   return (
-    <Popover
-      isOpen={isPopoverOpen}
-      positions={['right', 'top', 'bottom']}
-      onClickOutside={() => setIsPopoverOpen(false)}
-      padding={10}
-      content={({ position, childRect, popoverRect }: {
-        position: 'left' | 'right' | 'top' | 'bottom',
-        childRect: DOMRect,
-        popoverRect: DOMRect
-      }) => (
-        <ArrowContainer
-          position={position}
-          childRect={childRect}
-          popoverRect={popoverRect}
-          arrowColor={'blue'}
-          arrowSize={10}
-          arrowStyle={{ opacity: 0.7 }}
-        >
-          <span>Ok</span>
-        </ArrowContainer>
-      )}
-    >
-      {/* Trigger element that opens the popover */}
-      <span
-        onMouseEnter={() => setIsPopoverOpen(true)}
-        onMouseLeave={() => setIsPopoverOpen(false)}
-      >Hey</span>
-    </Popover>
+    <div>
+      <p>Client-side page content here.</p>
+    </div>
   );
 }
