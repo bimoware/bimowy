e317562411521854330050bd90dde0b1571f919e
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-04 15:32:19 +0000

    Delete get_invalid_method.ts

--- src/app/api/get_invalid_method.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export default function getInvalidMethodResponse(correctMethod: string) {
-	return NextResponse.json(
-		{ message: `The correct method for this route is ${correctMethod}.` },
-		{ status: 405 }
-	)
-}
