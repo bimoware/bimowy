b83b69a01df0fa4f3b377ce185b33b0ade4bde7f
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-04 15:32:15 +0000

    Switched to individual files for exercices instead of a single file

---
+++ src/app/api/db/addition.ts
@@ -0,0 +1,50 @@
+import { ExerciseGenerator } from '../defs'
+
+const ex = new ExerciseGenerator({
+	id: 'addition',
+	tags: ['basic-arithmetic'],
+	createdOn: 1740918617000,
+	generateSeed: function () {
+		const range = [1, 10]
+		const [n1, n2] = Array(2)
+			.fill(range)
+			.map(
+				(r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0]
+			)
+		return [n1, n2]
+	},
+	getContext: function ([n1, n2]: number[]) {
+		return [
+			{
+				type: 'p',
+				content: [
+					{
+						type: 'text',
+						text : `${n1} + ${n2} = `
+					},
+					{
+						type: 'input',
+						id: 'answer'
+					}
+				]
+			}
+		]
+	},
+	validateAnswers: function (
+		[n1, n2]: number[],
+		[answer1]: {
+			id: string
+			value: string
+		}[]
+	) {
+		return [
+			{
+				id: answer1.id,
+				is_correct:
+					String(answer1.value) === (n1 + n2).toString()
+			}
+		]
+	}
+})
+
+export default ex
