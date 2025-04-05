6a7fc34da90a6f3708b0f90f68e7f53e02400de2
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-05 01:19:06 +0000

    Hide description by default

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -44,7 +44,8 @@ function Card({ exercise }: { exercise: ExerciseGenerator }) {
   }, [randomFrom]);
 
   return (
-    <Link href={`/exercises/${exercise.id}`} className={`w-70 h-fit bg-neutral-700/20 p-4 rounded-xl
+    <Link href={`/exercises/${exercise.id}`} className={`w-fit h-fit bg-neutral-700/20 rounded-xl
+    p-4 px-8
     flex flex-col self-center gap-0.5
     transition select-none cursor-pointer
     hover:ring-2 hover:scale-105 hover:shadow-2xl
@@ -52,12 +53,13 @@ function Card({ exercise }: { exercise: ExerciseGenerator }) {
     group
     ${randomRotationClass}`}>
       {exercise.recent
-        && <span className='absolute px-3 py-1 bg-indigo-800 rounded-full -rotate-2 -m-6 font-bold
+        && <span className='absolute px-3 py-1 -m-8 group-hover:-rotate-5 group-hover:-translate-1
         transition-transform
-        group-hover:-rotate-5 group-hover:-translate-1'>NEW</span>
+        bg-indigo-800/70 rounded-full -rotate-2 font-bold
+        '>NEW</span>
       }
       <h4 className="self-center text-2xl font-bold">{exercise.name}</h4>
-      <span className="text-basic">{exercise.desc}</span>
+      <span className="self-center text-basic text-[0em] group-hover:text-sm transition-all text-neutral-50/50">{exercise.desc}</span>
       {/* Tags */}
       <div className="flex gap-2 flex-wrap mt-2 justify-center">
         {exercise.tags.map(tag => <span key={tag}
