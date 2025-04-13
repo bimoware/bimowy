e52cbdcf95983991a25ce6a8eba5876581e4270e
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-13 18:24:53 +0100

    Removed useless page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,3 @@
 export default function TestPage() {
-  return (
-    <div className="overflow-x-scroll">
-      {
-        Array
-          .from({ length: 200 })
-          .map((_, i) => <img
-            key={i}
-            width={300}
-            height={300}
-            alt={"Image"}
-            src={`https://skinmc.net/achievement/${i}/Too+curious/Discover+my+easter+egg`}
-          />)}
-    </div>
-  )
+  return <>test page</>
 }
