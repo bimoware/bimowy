775b7a73c69cd70eca25acc06fa3fb3267aed65d
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-23 12:37:02 +0100

    Fixed structure for exercises

--- src/app/api/db/multiplication.ts
+++ src/app/api/db/multiplication.ts
@@ -1,62 +1,61 @@
-import { ExerciseGenerator } from '../defs'
+import { ExerciseGenerator } from "../defs"
 
 type Seed = [n1: number, n2: number]
-type Answers = [n: number]
+type Answers = { answer: number }
 
 const ex = new ExerciseGenerator<Seed, Answers>({
-  id: 'multiplication',
-  nameLocales: { en: 'Multiplication', fr: 'Multiplication' },
-  tags: ['basic-arithmetic'],
-  createdOn: 2,
-  generateSeed: function () {
-    const range = [1, 10]
-    const [n1, n2] = Array(2)
-      .fill(range)
-      .map(
-        (r) =>
-          Math.floor(Math.random() * (r[1] - r[0])) + r[0]
-      )
-    return [n1, n2]
-  },
-  getContext: function ([n1, n2]: Seed) {
-    return [
-      {
-        type: 'p',
-        content: [
-          {
-            type: 'text',
-            text: `${n1} * ${n2} = `
-          },
-          {
-            type: 'input',
-            id: 'answer'
-          }
-        ]
-      }
-    ]
-  },
-  validateAnswers: function (
-    [n1, n2]: Seed,
-    [n]: Answers
-  ) {
-    return [ n == this.getSolution([n1, n2])[0] ]
-  },
-  getSolution: function (seed: Seed) {
-    return [seed[0] * seed[1]]
-  },
-  getDetailedSolution: function (seed: Seed) {
-    return [
-      {
-        type: 'p',
-        content: [
-          {
-            type: 'text',
-            text: `${seed.join(' * ')} = ${this.getSolution(seed)}`
-          }
-        ]
-      }
-    ]
-  }
+	id: "multiplication",
+  nameLocales: { en: "Multiplication", fr: "Multiplication" },
+	tags: ["basic-arithmetic"],
+	createdOn: 3,
+	generateSeed: function () {
+		const range = [1, 10]
+		const [n1, n2] = Array(2)
+			.fill(range)
+			.map((r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0])
+		return [n1, n2]
+	},
+	getContext: function ([n1, n2]: Seed) {
+		return [
+			{
+				type: "p",
+				content: [
+					{
+						type: "text",
+						text: `${n1} * ${n2} = `,
+					},
+					{
+						type: "input",
+						id: "answer",
+					},
+				],
+			},
+		]
+	},
+	validateAnswers: function ([n1, n2]: Seed, answers: Answers) {
+
+		return {
+			answer: answers["answer"] == this.getSolution([n1, n2])["answer"],
+		}
+	},
+	getSolution: function (seed: Seed) {
+		return {
+			answer: seed[0] * seed[1]
+		}
+	},
+	getDetailedSolution: function (seed: Seed) {
+		return [
+			{
+				type: "p",
+				content: [
+					{
+						type: "text",
+						text: `${seed.join(" * ")} = ${this.getSolution(seed)} (yes it's that simple)`,
+					},
+				],
+			},
+		]
+	},
 })
 
 export default ex
--- src/app/api/db/vector-norm.ts
+++ src/app/api/db/vector-norm.ts
@@ -1,124 +1,117 @@
-import { ExerciseGenerator, Language } from '../defs'
+import { ExerciseGenerator, Language } from "../defs"
 
 type Seed = [x1: number, x2: number, y1: number, y2: number]
-type Answers = [x: number, y: number]
+type Answers = { x: number; y: number }
 
 const ex = new ExerciseGenerator<Seed, Answers>({
-  id: 'from-points-to-vector',
-  nameLocales: {
-    en: 'Points to vector',
-    fr: 'Points à vecteur'
-  },
-  descLocales: {
-    en: 'Convert 2 points into a vector',
-    fr: 'Convertir 2 points en un vecteur'
-  },
-  tags: ['linear-algebra'],
-  createdOn: 4,
-  generateSeed: function () {
-    const range = [-9, 9]
-    const [x1, y1, x2, y2] = Array(4)
-      .fill(range)
-      .map(
-        (r) =>
-          Math.floor(Math.random() * (r[1] - r[0])) + r[0]
-      )
-    return [x1, y1, x2, y2]
-  },
-  getContext: function (
-    [x1, y1, x2, y2]: number[],
-    lang: Language
-  ) {
-    return [
-      {
-        type: 'p',
-        content: [
-          {
-            type: 'text',
-            text: {
-              en: 'The vector that goes from',
-              fr: 'Le vecteur qui commence de'
-            }[lang]
-          },
-          {
-            type: 'text',
-            text: `A(${x1},${y1})`,
-            extra: ['mono']
-          },
-          {
-            type: 'text',
-            text: { en: 'to', fr: 'à' }[lang]
-          },
-          {
-            type: 'text',
-            text: `B(${x2},${y2})`,
-            extra: ['mono']
-          },
-          {
-            type: 'text',
-            text: { en: 'is:', fr: 'est:' }[lang]
-          }
-        ]
-      },
-      {
-        type: 'p',
-        content: [
-          {
-            type: 'text',
-            text: '\\overrightarrow{AB}',
-            extra: ['latex']
-          },
-          {
-            type: 'text',
-            text: '= <',
-            extra: ['mono']
-          },
-          {
-            type: 'input'
-          },
-          {
-            type: 'text',
-            text: ',',
-            extra: ['mono']
-          },
-          {
-            type: 'input'
-          },
-          {
-            type: 'text',
-            text: '>',
-            extra: ['mono']
-          }
-        ]
-      }
-    ]
-  },
-  validateAnswers: function (
-    [x1, x2, y1, y2]: Seed,
-    [x, y]: Answers
-  ) {
-    const [correctX, correctY] = this.getSolution([x1, x2, y1, y2])
-    return [
-      x == correctX,
-      y == correctY
-    ]
-  },
-  getSolution: function ([x1, y1, x2, y2]: number[]) {
-    return [x2 - x1, y2 - y1]
-  },
-  getDetailedSolution: function (seed: number[]) {
-    return [
-      {
-        type: 'p',
-        content: [
-          {
-            type: 'text',
-            text: '[Solution Here]'
-          }
-        ]
-      }
-    ]
-  }
+	id: "from-points-to-vector",
+	nameLocales: {
+		en: "Points to vector",
+		fr: "Points à vecteur",
+	},
+	descLocales: {
+		en: "Convert 2 points into a vector",
+		fr: "Convertir 2 points en un vecteur",
+	},
+	tags: ["linear-algebra"],
+	createdOn: 4,
+	generateSeed: function () {
+		const range = [-9, 9]
+		const [x1, y1, x2, y2] = Array(4)
+			.fill(range)
+			.map((r) => Math.floor(Math.random() * (r[1] - r[0])) + r[0])
+		return [x1, y1, x2, y2]
+	},
+	getContext: function ([x1, y1, x2, y2]: number[], lang: Language) {
+		return [
+			{
+				type: "p",
+				content: [
+					{
+						type: "text",
+						text: {
+							en: "The vector that goes from",
+							fr: "Le vecteur qui commence de",
+						}[lang],
+					},
+					{
+						type: "text",
+						text: `A(${x1},${y1})`,
+						extra: ["mono"],
+					},
+					{
+						type: "text",
+						text: { en: "to", fr: "à" }[lang],
+					},
+					{
+						type: "text",
+						text: `B(${x2},${y2})`,
+						extra: ["mono"],
+					},
+					{
+						type: "text",
+						text: { en: "is:", fr: "est:" }[lang],
+					},
+				],
+			},
+			{
+				type: "p",
+				content: [
+					{
+						type: "text",
+						text: "\\overrightarrow{AB}",
+						extra: ["latex"],
+					},
+					{
+						type: "text",
+						text: "= <",
+						extra: ["mono"],
+					},
+					{
+						type: "input",
+						id: "x",
+					},
+					{
+						type: "text",
+						text: ",",
+						extra: ["mono"],
+					},
+					{
+						type: "input",
+						id: "y",
+					},
+					{
+						type: "text",
+						text: ">",
+						extra: ["mono"],
+					},
+				],
+			},
+		]
+	},
+	validateAnswers: function ([x1, x2, y1, y2]: Seed, { x, y }: Answers) {
+		const solution = this.getSolution([x1, x2, y1, y2])
+		return {
+			x: x == solution.x,
+			y: y == solution.y,
+		}
+	},
+	getSolution: function ([x1, y1, x2, y2]: number[]) {
+		return { x: x2 - x1, y: y2 - y1 }
+	},
+	getDetailedSolution: function (seed: number[]) {
+		return [
+			{
+				type: "p",
+				content: [
+					{
+						type: "text",
+						text: "[Solution Here]",
+					},
+				],
+			},
+		]
+	},
 })
 
 export default ex
