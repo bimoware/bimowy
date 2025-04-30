7f20f11afaf919874a9a424b72955681f55b12c9
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-30 03:00:15 +0100

    Added options + fixed creation date

--- src/app/api/db/vector_norm.ts
+++ src/app/api/db/vector_norm.ts
@@ -2,18 +2,34 @@ import { ExerciseGenerator, Language } from "../defs"
 
 type Seed = [x: number, y: number]
 type Answers = { answer: number }
+type Options = { min: number; max: number }
 
 const getExercise = (id: string) =>
-	new ExerciseGenerator<Seed, Answers>({
+	new ExerciseGenerator<Seed, Answers, Options>({
 		id,
-		beta: true,
 		nameLocales: { fr: "Norme de vecteur", en: "Vector Norm" },
 		descLocales: {
 			fr: "Ou sa 'longeur'",
-			en: "i.e it's 'length'.",
+			en: "i.e it's 'length'."
 		},
-		createdOn: 6,
+		createdOn: 4,
 		tags: ["linear-algebra"],
+		options: [
+			{
+				type: "number",
+				id: "min",
+				title: "Minimum",
+				defaultValue: 0,
+				min: 0
+			},
+			{
+				type: "number",
+				id: "max",
+				title: "Maximum",
+				defaultValue: 10,
+				max: 10
+			}
+		],
 		generateSeed() {
 			const range = [-9, 9]
 			const [x, y] = Array(4)
@@ -28,34 +44,34 @@ const getExercise = (id: string) =>
 					content: [
 						{
 							type: "text",
-							text: "The norm of the vector",
+							text: "The norm of the vector"
 						},
 						{
 							type: "text",
 							text: `<${x},${y}>`,
-							extra: ["mono"],
+							extra: ["mono"]
 						},
 						{
 							type: "text",
-							text: " = ",
+							text: " = "
 						},
 						{
 							type: "input",
-							id: "answer",
-						},
-					],
-				},
+							id: "answer"
+						}
+					]
+				}
 			]
 		},
 		validateAnswers([x, y], answers) {
 			const { answer } = this.getSolution([x, y])
 			return {
-				answer: Math.abs(answer - answers["answer"]) <= 0.01,
+				answer: Math.abs(answer - answers["answer"]) <= 0.01
 			}
 		},
 		getSolution([x, y]: Seed) {
 			return {
-				answer: Number(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)).toFixed(2)),
+				answer: Number(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)).toFixed(2))
 			}
 		},
 		getDetailedSolution(seed: number[]) {
@@ -65,12 +81,12 @@ const getExercise = (id: string) =>
 					content: [
 						{
 							type: "text",
-							text: "[Solution Here]",
-						},
-					],
-				},
+							text: "[Solution Here]"
+						}
+					]
+				}
 			]
-		},
+		}
 	})
 
 export default getExercise
