3ad75ac24707bbd48a70c9e17304d6468af1ab22
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-30 03:01:19 +0100

    Added cool functions cuz yea

--- src/app/api/util.ts
+++ src/app/api/util.ts
@@ -1,17 +1,25 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server"
 
 export function Error(message: string) {
-  return NextResponse.json({
-    ok: false,
-    status: 400,
-    message
-  })
+	return NextResponse.json({
+		ok: false,
+		status: 400,
+		message
+	})
 }
 
 export function Success(data: any) {
-  return NextResponse.json({ ok: true, status: 200, data })
+	return NextResponse.json({ ok: true, status: 200, data })
 }
 
-export function isValidLang(lang: string){
-  return lang == "fr" || lang == "en"
+export function isValidLang(lang: string) {
+	return lang == "fr" || lang == "en"
+}
+
+export function randomFromRange(min: number, max: number) {
+	return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+export function randomFrom(arr: any[]) {
+	return arr[Math.floor(Math.random() * arr.length)]
 }
