37fcdfeb97a7034d06ffc9255fd33a50d332dfa5
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-30 03:02:22 +0100

    Fixed PageNotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { useTranslations } from 'next-intl'
 
 export const metadata: Metadata = {
   title: 'Page not found',
@@ -6,12 +7,12 @@ export const metadata: Metadata = {
 }
 
 export default function NotFound() {
+  const t = useTranslations('404')
   return (
     <div className='w-full h-full flex flex-col items-center justify-center'>
-      <h1>Page Not Found (404)</h1>
+      <h1>{t('PageNotFound')} (404)</h1>
       <p>
-        Sorrwy, couldn&apos;t get to where you wanted to go
-        😔..
+        {t('PageNotFoundDesc')} 😔..
       </p>
     </div>
   )
