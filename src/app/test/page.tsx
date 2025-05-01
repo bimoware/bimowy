dda7e5b0ada25336023de12971ab7155086bd1eb
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-05-01 13:02:40 +0100

    Removed testpage, it was fun tho'

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,5 @@
 "use client";
-import { useState } from "react"
 
 export default function TestPage() {
-  const [toggled, setToggled] = useState(false)
-  return <div
-  aria-checked={toggled}
-  className={`m-10 h-10 w-20
-  shadow-[inset_0_20px_10px_rgba(0,0,0,0.5)]
-  rounded-full flex items-center p-1 relative
-  justify-start aria-checked:justify-end
-  transition-all`}
-    onClick={() => setToggled(prev => !prev)}>
-    <div className="h-full aspect-square bg-white rounded-full transition-all"></div>
-  </div>
+  return <>k</>
 }
