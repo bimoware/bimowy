1e376732a9071023a14fbeb68bea1e5226d83674
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-05-01 09:09:16 -0400

    Added lessons & favorites to sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -24,6 +24,24 @@ export default function SideBar() {
             path: 'exercises',
             label: 'Exercises'
           }
+        },
+        {
+          id: "lessons",
+          data: {
+            beta: true,
+            icon: '/svgs/lesson.svg',
+            path: 'lessons',
+            label: 'Lessons'
+          }
+        },
+        {
+          id: "favorites",
+          data: {
+            beta: true,
+            icon: '/svgs/bookmark.svg',
+            path: 'favorites',
+            label: 'Favorites'
+          }
         }
       ].map((btn) => (
         <SideBarIcon {...btn.data} key={btn.id} />
@@ -35,24 +53,27 @@ export default function SideBar() {
 function SideBarIcon({
   icon,
   path,
-  label
+  label,
+  beta
 }: {
   icon: string
   path: string
   label: string
+  beta?: boolean
 }) {
   const pathname = usePathname().split('/')[1] || '' // Get the current path without the leading slash (/exercices/add -> exercices)
   const isActive = pathname === path // Check if the current path matches the icon's path
 
   const myDiv = (
     <div
-      className={`aspect-square rounded-xl p-2 m-1 transition-transform hover:translate-x-1 hover:scale-105
-            ${
-              isActive
-                ? 'bg-neutral-50/5'
-                : 'hover:bg-neutral-50/5 opacity-90'
-            }
-                `}
+      className={`aspect-square rounded-xl p-2 m-1
+        duration-150 hover:translate-x-1 hover:scale-105
+            ${beta ? "!opacity-40" : ""}
+            ${isActive
+          ? 'bg-neutral-50/5'
+          : 'hover:bg-neutral-50/5 opacity-90'
+        }
+            `}
     >
       <Image
         src={icon}
