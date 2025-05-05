6352fc16fee3ffa9decfd6a760155396fa12c159
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-05-05 05:31:49 +0100

    Added caching for SVGs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from 'next'
-import createNextIntlPlugin from 'next-intl/plugin'
+import type { NextConfig } from "next"
+import createNextIntlPlugin from "next-intl/plugin"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	async headers() {
+		return [
+			{
+				source: "/svgs/:path*",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable"
+					}
+				]
+			}
+		]
+	}
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
