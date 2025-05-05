2467cb8f0af7ebbe96bc114c2a16b8f11d14f07e
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-05-05 07:35:48 +0100

    Added way more icons to sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,52 +1,111 @@
 'use client'
 
-import { Bloc } from '@cpn/Bloc'
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 
 export default function SideBar() {
   return (
-    <Bloc type='sidebar'>
-      {[
-        {
-          id: 'home',
-          data: {
-            icon: '/svgs/home.svg',
-            path: '',
-            label: 'Home'
+    <div className='w-24 h-full p-4 flex flex-col justify-between shrink-0 gap-4'>
+      <div className='bg-neutral-900 p-1 w-full h-fit
+    rounded-3xl
+    overflow-y-auto
+    flex flex-col items-center'>
+        {[
+          {
+            id: 'home',
+            data: {
+              icon: '/svgs/home.svg',
+              path: '',
+              label: 'Home'
+            }
+          },
+          {
+            id: "discover",
+            data: {
+              beta: true,
+              icon: '/svgs/path.svg',
+              path: 'discover',
+              label: 'Discover'
+            }
+          },
+          {
+            id: 'sandbox',
+            data: {
+              icon: '/svgs/sandbox.svg',
+              path: 'sandbox',
+              label: 'Sandbox'
+            }
+          },
+          {
+            id: 'draft',
+            data: {
+              beta: true,
+              icon: '/svgs/draft.svg',
+              path: 'draft',
+              label: 'Draft'
+            }
+          },
+
+          {
+            id: 'health',
+            data: {
+              beta: true,
+              icon: '/svgs/health.svg',
+              path: 'health',
+              label: 'Health'
+            }
           }
-        },
-        {
-          id: 'ex',
-          data: {
-            icon: '/svgs/exercises.svg',
-            path: 'exercises',
-            label: 'Exercises'
+        ].map((btn) => (
+          <SideBarIcon {...btn.data} key={btn.id} />
+        ))}
+      </div>
+      <div className='bg-neutral-900 p-2 w-full h-fit
+      rounded-3xl
+      overflow-y-auto
+      flex flex-col items-center'>
+        {[
+          {
+            id: "progress",
+            data: {
+              beta: true,
+              icon: '/svgs/progress.svg',
+              path: 'progress',
+              label: 'Progress'
+            }
+          },
+          // {
+          //   id: "history",
+          //   data: {
+          //     beta: true,
+          //     icon: '/svgs/history.svg',
+          //     path: 'history',
+          //     label: 'History'
+          //   }
+          // },
+          {
+            id: 'login',
+            data: {
+              beta: true,
+              icon: '/svgs/login.svg',
+              path: 'login',
+              label: 'Login'
+            }
+          },
+          {
+            id: 'settings',
+            data: {
+              beta: true,
+              icon: '/svgs/settings.svg',
+              path: 'settings',
+              label: 'Settings'
+            }
           }
-        },
-        {
-          id: "lessons",
-          data: {
-            beta: true,
-            icon: '/svgs/lesson.svg',
-            path: 'lessons',
-            label: 'Lessons'
-          }
-        },
-        {
-          id: "favorites",
-          data: {
-            beta: true,
-            icon: '/svgs/bookmark.svg',
-            path: 'favorites',
-            label: 'Favorites'
-          }
-        }
-      ].map((btn) => (
-        <SideBarIcon {...btn.data} key={btn.id} />
-      ))}
-    </Bloc>
+        ].map((btn) => (
+          <SideBarIcon {...btn.data} key={btn.id} />
+        ))}
+      </div>
+    </div>
   )
 }
 
@@ -67,15 +126,17 @@ function SideBarIcon({
   const myDiv = (
     <div
       className={`aspect-square rounded-xl p-2 m-1
-        duration-150 hover:translate-x-1 hover:scale-105
-            ${beta ? "!opacity-40" : ""}
-            ${isActive
-          ? 'bg-neutral-50/5'
-          : 'hover:bg-neutral-50/5 opacity-90'
+        duration-150 hover:translate-x-0.5 hover:scale-105
+            ${beta
+          ? "!opacity-30 cursor-not-allowed"
+          : isActive
+            ? 'bg-neutral-50/5'
+            : 'hover:bg-neutral-50/5 opacity-90'
         }
-            `}
+          group`}
     >
       <Image
+        className='group-hover:rotate-2 group-hover:scale-110 duration-150'
         src={icon}
         alt={label}
         width={30}
