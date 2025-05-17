2ddbb74cd148660724e4df998e7464a677a80f60
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-05-17 02:38:57 +0100

    Added tag system (INCOMPLETE)

---
+++ db/tags.ts
@@ -0,0 +1,16 @@
+export default [
+	{
+		id: 'basic-arithmetic',
+		names: {
+			fr: 'Arithmétique Elementaire',
+			en: 'Basic Arithmetic'
+		}
+	},
+	{
+		id: 'plane',
+		names: {
+			fr: 'Plan 2D',
+			en: '2D Plane'
+		}
+	},
+] as const
