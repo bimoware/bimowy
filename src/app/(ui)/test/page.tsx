f0274ee01b361448d83b97af0735c5cf7cbbc86c
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-06-29 00:36:21 +0100

    ▫️Minor testing

--- src/app/(ui)/test/page.tsx
+++ src/app/(ui)/test/page.tsx
@@ -1,9 +1,24 @@
 import { generateMetadataUtil } from "@/utils/sidebar";
+import Plane from "@cpn/Plane";
 
 export async function generateMetadata() {
   return await generateMetadataUtil('test')
 }
 
 export default async function TestPage() {
-  return <></>
+  return <div className="p-1
+  w-1/3
+  aspect-square
+  bg-white/5
+  rounded-2xl
+  flex flex-col items-center">
+    <Plane elems={[
+      {
+        type: "point",
+        id: "A",
+        x: 2,
+        y: 4
+      }
+    ]} />
+  </div>
 }
