35675b3eaa4037f65f85e323bfc7ffff30272073
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-06-30 04:05:43 +0100

    ▪️ Minor tests

--- src/app/(ui)/test/page.tsx
+++ src/app/(ui)/test/page.tsx
@@ -1,24 +1,53 @@
-import { generateMetadataUtil } from "@/utils/sidebar";
-import Plane from "@cpn/Plane";
+import { generateMetadataUtil } from "@/utils/sidebar"
+import Plane from "@cpn/math/Plane"
 
-export async function generateMetadata() {
-  return await generateMetadataUtil('test')
+export function generateMetadata() {
+	return generateMetadataUtil("test")
 }
 
-export default async function TestPage() {
-  return <div className="p-1
-  w-1/3
-  aspect-square
-  bg-white/5
-  rounded-2xl
-  flex flex-col items-center">
-    <Plane elems={[
-      {
-        type: "point",
-        id: "A",
-        x: 2,
-        y: 4
-      }
-    ]} />
-  </div>
+export default function TestPage() {
+	return <div className="p-1
+	w-80
+	aspect-square
+	bg-white/5
+	rounded-2xl">
+		<Plane
+			ranges={{
+				x: { min: -7, max: 7 },
+				y: { min: -7, max: 7 }
+			}}
+			elems={[
+				{
+					type: "vector",
+					id: "u",
+					x1: -3, y1: 0,
+					x2: -1, y2: 2
+				},
+				{
+					type: "vector",
+					id: "v",
+					x1: 2, y1: -4,
+					x2: 3, y2: 2
+				},
+				{
+					type: "vector",
+					id: "w",
+					x1: -2, y1: -2,
+					x2: -2, y2: -3
+				},
+				{
+					type: "vector",
+					id: "x",
+					x1: 0, y1: 0,
+					x2: 2, y2: 3
+				},
+				{
+					type: "point",
+					id: "A",
+					x: 2,
+					y: 3
+				},
+			]}
+		/>
+	</div>
 }
