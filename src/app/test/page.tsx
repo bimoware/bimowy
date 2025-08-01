478920212176214fadbadd4d75772f803f0f5e14
Malik Lahlou <71925280+bimoware@users.noreply.github.com>
2025-08-01 05:52:40 +0200

    🦠 Removed uselesss test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,9 @@
 import { generateMetadataUtil } from "@cpn/sidebars/main"
-import InteractivePlane from "@cpn/math/plane/InteractivePlane"
 
 export function generateMetadata() {
 	return generateMetadataUtil("test")
 }
 
 export default function TestPage() {
-	return <section className="w-full h-full">
-		<InteractivePlane />
-	</section>
+	return 
 }
