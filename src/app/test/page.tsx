9c1a68e07a190432580b6eaa46676ec074bdfe16
Malik Lahlou <71925280+bimoware@users.noreply.github.com>
2025-10-31 19:45:48 +0100

    🗑️d useless test page

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NumberInput } from "@/cpn/main/number-input";
-
-export default function TestPage() {
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <NumberInput />
-    </div>
-  );
-}
